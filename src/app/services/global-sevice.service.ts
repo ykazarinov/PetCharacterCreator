5e2fba87568955467658ce3bec512990791642a9
ykazarinov <kazarinov.yanek@gmail.com>
2022-10-31 12:19:14 +0100

    server_url: string = https://serene-pasteur.82-165-57-61.plesk.page/

--- src/app/services/global-sevice.service.ts
+++ src/app/services/global-sevice.service.ts
@@ -174,8 +174,8 @@ export interface PetBackground{
 })
 export class GlobalSeviceService {
 
-  server_url: string = process.env.server_url
-  // server_url: string = 'https://serene-pasteur.82-165-57-61.plesk.page'
+  // server_url: string = process.env.server_url || 'http://localhost:8000/' || 'https://serene-pasteur.82-165-57-61.plesk.page/';
+  server_url: string = 'https://serene-pasteur.82-165-57-61.plesk.page/'
   // server_url: string = 'http://localhost:8000/';
   server_media_url: string = 'storage/';
 
