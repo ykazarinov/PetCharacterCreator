01364c939b777d33123545e43ad72f9c86c8ca80
ykazarinov <kazarinov.yanek@gmail.com>
2022-10-31 13:58:20 +0100

    http => https

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -16,7 +16,8 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  // baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://petcharactercreator.vercel.app/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
